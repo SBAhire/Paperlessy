5e4e5685bf9b36cad4e65365d5754997e797e7a8
SBAhire <sohamahireaquar2@gmail.com>
2021-05-09 17:39:39 +0530

    Added some more functionality

--- sticky_notes/urls.py
+++ sticky_notes/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-extra-numeral-data-handler/', admin.site.urls),
     path('user/',include('users.urls')),
     path('',include('tasks.urls')),
     
